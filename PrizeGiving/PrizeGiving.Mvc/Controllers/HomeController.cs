98e67327a50a15d03ecff6d9e2baa36415b84dc9
Mark Pearl <markpearl@gmail.com>
2014-05-16 16:29:56 +0200

    Work on #2 - add mvc project and got basic structure kind of working

---
+++ PrizeGiving/PrizeGiving.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PrizeGiving.Models;
+
+namespace PrizeGiving.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly GroupEventsQuery _groupEventsQuery;
+
+        public HomeController() : this(new MeetupGroupEventsQuery())
+        {
+        }
+
+        public HomeController(GroupEventsQuery groupEventsQuery)
+        {
+            _groupEventsQuery = groupEventsQuery;
+        }
+
+        public ActionResult Index()
+        {
+            var events = _groupEventsQuery.GetEventsForGroup("DeveloperUG");
+            var indexModel = new IndexModel(events);
+
+            return View(indexModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    public class IndexModel
+    {
+        public SelectList Events { get; set; }
+        public MeetupEvent MeetupEvent { get; set; }
+
+        public IndexModel(IEnumerable<MeetupEvent> events)
+        {
+            Events = new SelectList(events);
+        }
+    }
+}
